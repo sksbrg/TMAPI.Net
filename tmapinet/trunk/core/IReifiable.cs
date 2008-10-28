8d91064707981e7e9f4d4302a5c0efd5fdca7f9e
mhoyer <mhoyer@c94865a6-8e29-11dd-9ae4-9bd23fdad81d>
2008-10-28 22:40:12 +0000

    * fixed StyleCop warnings for IReifiable.cs   * SA1500: If a statement spans multiple lines, the opening curly bracket must be placed on its own line.

--- tmapinet/trunk/core/IReifiable.cs
+++ tmapinet/trunk/core/IReifiable.cs
@@ -1,21 +1,27 @@
-namespace org.tmapi.core {
+namespace org.tmapi.core
+{
 	/// <summary>
 	///     Indicates that a <see cref="T:org.tmapi.core.IConstruct"/> is reifiable.
-    ///     Every Topic Maps construct that is not a <see cref="T:org.tmapi.core.ITopic"/> is reifiable.
+	///     Every Topic Maps construct that is not a <see cref="T:org.tmapi.core.ITopic"/> is reifiable.
 	/// </summary>
-	public interface IReifiable : IConstruct {
-        #region Properties
-        /// <summary>
-        ///     Gets or sets the reifier of this construct.
-        /// </summary>
-        /// <remarks>
-        ///     <list type="bullet">
-        ///         <item>If this construct is not reified <c>null</c> is returned.</item>
-        ///         <item>If the reifier is set to <c>null</c> an existing reifier should be removed.</item>
-        ///         <item>The reifier of this construct MUST NOT reify another information item.</item>
-        ///     </list>
-        /// </remarks>
-        ITopic Reifier { get; set; } 
-        #endregion
+	public interface IReifiable : IConstruct
+	{
+		#region Properties
+		/// <summary>
+		///     Gets or sets the reifier of this construct.
+		/// </summary>
+		/// <remarks>
+		///     <list type="bullet">
+		///         <item>If this construct is not reified <c>null</c> is returned.</item>
+		///         <item>If the reifier is set to <c>null</c> an existing reifier should be removed.</item>
+		///         <item>The reifier of this construct MUST NOT reify another information item.</item>
+		///     </list>
+		/// </remarks>
+		ITopic Reifier
+		{
+			get;
+			set;
+		}
+		#endregion
 	}
 }
