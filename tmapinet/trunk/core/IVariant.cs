dbb3e4376e3876c33453282f698d87061cc973e5
Marcel Hoyer <mhoyer@users.noreply.github.com>
2008-10-28 23:19:02 +0000

    * fixed StyleCop warnings for IVariant.cs   * SA1500: If a statement spans multiple lines, the opening curly bracket must be placed on its own line.   * SA1623: The property's documentation summary text must begin with: Gets * fixed documentation of Scope property   * moved text to <remarks> tag

--- tmapinet/trunk/core/IVariant.cs
+++ tmapinet/trunk/core/IVariant.cs
@@ -1,28 +1,38 @@
 using System.Collections.ObjectModel;
 
-namespace org.tmapi.core {
+namespace org.tmapi.core
+{
 	/// <summary>
 	///     Represents a 
 	///     <a href="http://www.isotopicmaps.org/sam/sam-model/#sect-variant">variant item</a>.
 	/// </summary>
-	public interface IVariant : IDatatypeAware {
-        #region Properties
-        /// <summary>
-        ///     Returns the <see cref="T:org.tmapi.core.IName"/> to which this variant belongs.
-        /// </summary>
-        /// <returns>
-        ///     The <see cref="T:org.tmapi.core.IName"/> to which this variant belongs.
-        /// </returns>
-        new IName Parent { get; }
+	public interface IVariant : IDatatypeAware
+	{
+		#region Properties
+		/// <summary>
+		///     Gets the <see cref="T:org.tmapi.core.IName"/> to which this variant belongs.
+		/// </summary>
+		/// <returns>
+		///     The <see cref="T:org.tmapi.core.IName"/> to which this variant belongs.
+		/// </returns>
+		new IName Parent
+		{
+			get;
+		}
 
-        /// <summary>
-        ///     Returns the scope of this variant.
-        ///     The returned scope is a true superset of the parent's scope.
-        /// </summary>
-        /// <returns>
-        ///     An unmodifiable set of <see cref="T:org.tmapi.core.ITopic"/>s which define the scope.
-        /// </returns>
-        new ReadOnlyCollection<ITopic> Scope { get; }
-        #endregion
+		/// <summary>
+		///     Gets the scope of this variant.
+		/// </summary>
+		/// <remarks>
+		///     The returned scope is a true superset of the parent's scope.
+		/// </remarks>
+		/// <returns>
+		///     An unmodifiable set of <see cref="T:org.tmapi.core.ITopic"/>s which define the scope.
+		/// </returns>
+		new ReadOnlyCollection<ITopic> Scope
+		{
+			get;
+		}
+		#endregion
 	}
 }
