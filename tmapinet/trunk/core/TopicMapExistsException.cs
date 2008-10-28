7dd63f0b669e2c638d947826dd1ec20bf9c10f96
Marcel Hoyer <mhoyer@users.noreply.github.com>
2008-10-28 23:33:53 +0000

    * fixed StyleCop warnings for TopicMapExistsException.cs   * SA1012: Invalid spacing around the opening curly bracket.   * SA1013: Invalid spacing around the closing curly bracket.   * SA1500: If a statement spans multiple lines, the opening curly bracket must be placed on its own line.   * SA1502: The constructor must not be placed on a single line. The opening and closing curly brackets must each be placed on their own line.   * SA1505: An opening curly bracket must not be followed by a blank line.   * SA1508: A closing curly bracket must not be preceded by a blank line.   * SA1642: The documentation text within the constructor's summary tag must begin with the text: Initializes a new instance of the TopicMapExistsException class.

--- tmapinet/trunk/core/TopicMapExistsException.cs
+++ tmapinet/trunk/core/TopicMapExistsException.cs
@@ -1,61 +1,56 @@
 using System;
 
-namespace org.tmapi.core {
+namespace org.tmapi.core
+{
 	/// <summary>
 	///     Exception thrown when an attempt is made to create a new <see cref="T:org.tmapi.core.ITopicMap"/> under 
 	///     a storage address (an IRI) that is already assigned to another <see cref="T:org.tmapi.core.ITopicMap"/> 
 	///     in the same <see cref="T:org.tmapi.core.ITopicMapSystem"/>.
 	/// </summary>
-    [Serializable]
-    public class TopicMapExistsException : TMAPIException
-    {
-        /// <summary>
-        ///     Constructs a new exception.
-        /// </summary>
-        public TopicMapExistsException() {}
+	[Serializable]
+	public class TopicMapExistsException : TMAPIException
+	{
+		#region constructor logic
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TopicMapExistsException"/> class.
+		/// </summary>
+		public TopicMapExistsException()
+		{
+		}
 
-        /// <summary>
-        ///     Constructs a new exception with the specified detail <paramref name="message"/>.
-        /// </summary>
-        /// <param name="message">
-        ///     The detail message.
-        /// </param>
-        public TopicMapExistsException(string message)
-            : base(message)
-        {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TopicMapExistsException"/> class with the specified detail <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The detail message.</param>
+		public TopicMapExistsException(string message)
+			: base(message)
+		{
+		}
 
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TopicMapExistsException"/> class that wraps another exception with the specified detail <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The detail message.</param>
+		/// <param name="innerException">Exception to be wrapped.</param>
+		public TopicMapExistsException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
 
-        /// <summary>
-        ///     Constructs a new exception that wraps another exception with the specified detail <paramref name="message"/>.
-        /// </summary>
-        /// <param name="message">
-        ///     The detail message.    
-        /// </param>
-        /// <param name="innerException">
-        ///     Exception to be wrapped.
-        /// </param>
-        public TopicMapExistsException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-
-        }
-
-        /// <summary>
-        ///     Constructs a new exception with serialized data. 
-        /// </summary>
-        /// <param name="info">
-        ///     The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object 
-        ///     data about the exception being thrown.
-        /// </param>
-        /// <param name="context">
-        ///     The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual 
-        ///     information about the source or destination.
-        /// </param>
-        protected TopicMapExistsException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
-            : base(info, context)
-        {
-
-        }
-    }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TopicMapExistsException"/> class with serialized data.
+		/// </summary>
+		/// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object
+		/// data about the exception being thrown.</param>
+		/// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual
+		/// information about the source or destination.</param>
+		/// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
+		/// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
+		protected TopicMapExistsException(
+			System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+			: base(info, context)
+		{
+		}
+		#endregion
+	}
 }
