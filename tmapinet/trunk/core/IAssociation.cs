115c7633942b794c1ff4b6641b71a939626ece8f
mhoyer <mhoyer@c94865a6-8e29-11dd-9ae4-9bd23fdad81d>
2008-10-28 19:42:39 +0000

    * fixed StyleCop warnings for IAssociation.cs   * SA1500: If a statement spans multiple lines, the opening curly bracket must be placed on its own line.   * SA1633: The file has no header, the header Xml is invalid, or the header is not located at the top of the file.

--- tmapinet/trunk/core/IAssociation.cs
+++ tmapinet/trunk/core/IAssociation.cs
@@ -1,71 +1,84 @@
 using System.Collections.ObjectModel;
 
-namespace org.tmapi.core {
+namespace org.tmapi.core
+{
 	/// <summary>
 	///     Represents an <a href="http://www.isotopicmaps.org/sam/sam-model/#sect-association">
 	///     association item</a>.
 	/// </summary>
-	public interface IAssociation : IReifiable, ITyped, IScoped {
-        #region Properties
-        /// <summary>
-        ///     Returns the <see cref="T:org.tmapi.core.ITopicMap"/> to which this association belongs.
-        /// </summary>
-        /// <returns>
-        ///     The <see cref="T:org.tmapi.core.ITopicMap"/> to which this association belongs.
-        /// </returns>
-        new ITopicMap Parent { get; }
+	public interface IAssociation : IReifiable, ITyped, IScoped
+	{
+		#region Properties
+		/// <summary>
+		///    Gets the <see cref="T:org.tmapi.core.ITopicMap"/> this association belongs to.
+		/// </summary>
+		/// <returns>
+		///     The <see cref="T:org.tmapi.core.ITopicMap"/> this association belongs to.
+		/// </returns>
+		new ITopicMap Parent
+		{
+			get;
+		}
 
-        /// <summary>
-        ///     Returns the roles participating in this association.
-        ///     The return value may be empty but must never be <c>null</c>.
-        /// </summary>
-        /// <returns>
-        ///     An unmodifiable set of <see cref="T:org.tmapi.core.IRole"/>s.
-        /// </returns>
-        ReadOnlyCollection<IRole> Roles { get; }
+		/// <summary>
+		///     Gets the roles participating in this association.
+		/// </summary>
+		/// <remarks>
+		///     The return value may be empty but must never be <c>null</c>.
+		/// </remarks>
+		/// <returns>
+		///     An unmodifiable set of <see cref="T:org.tmapi.core.IRole">IRoles</see>.
+		/// </returns>
+		ReadOnlyCollection<IRole> Roles
+		{
+			get;
+		}
 
-        /// <summary>
-        ///     Returns the role types participating in this association.
-        ///     The return value may be empty but must never be <c>null</c>.
-        /// </summary>
-        /// <returns>
-        ///     An unmodifiable set of role types.
-        /// </returns>
-        ReadOnlyCollection<ITopic> RoleTypes { get; } 
-        #endregion
+		/// <summary>
+		///     Gets the role types participating in this association.
+		///     The return value may be empty but must never be <c>null</c>.
+		/// </summary>
+		/// <returns>
+		///     An unmodifiable set of role types.
+		/// </returns>
+		ReadOnlyCollection<ITopic> RoleTypes
+		{
+			get;
+		}
+		#endregion
 
-        #region Methods
-        /// <summary>
-        ///     Creates a new <see cref="T:org.tmapi.core.IRole"/> representing a role in this association.
-        /// </summary>
-        /// <param name="type">
-        ///     The role type; must not be <c>null</c>.
-        /// </param>
-        /// <param name="player">
-        ///     The role player; must not be <c>null</c>.
-        /// </param>
-        /// <returns>
-        ///     A newly created association role.
-        /// </returns>
-        /// <exception cref="T:org.tmapi.core.ModelConstraintException">
-        ///     If the <paramref name="type"/> or <paramref name="player"/> is <c>null</c>.
-        /// </exception>
-        IRole CreateRole(ITopic type, ITopic player);
+		#region Methods
+		/// <summary>
+		///     Creates a new <see cref="T:org.tmapi.core.IRole"/> representing a role in this association.
+		/// </summary>
+		/// <param name="type">
+		///     The role type; must not be <c>null</c>.
+		/// </param>
+		/// <param name="player">
+		///     The role player; must not be <c>null</c>.
+		/// </param>
+		/// <returns>
+		///     A newly created association role.
+		/// </returns>
+		/// <exception cref="T:org.tmapi.core.ModelConstraintException">
+		///     If the <paramref name="type"/> or <paramref name="player"/> is <c>null</c>.
+		/// </exception>
+		IRole CreateRole(ITopic type, ITopic player);
 
-        /// <summary>
-        ///     Returns all roles with the specified <c>type</c>.
-        ///     The return value may be empty but must never be <c>null</c>.
-        /// </summary>
-        /// <param name="type">
-        ///     The type of the <see cref="T:org.tmapi.core.IRole"/> instances to be returned, must not be <c>null</c>.
-        /// </param>
-        /// <returns>
-        ///     An unmodifiable (maybe empty) set of roles with the specified <c>type</c> property.
-        /// </returns>
-        /// <exception cref="T:System.ArgumentNullException">
-        ///     If the <paramref name="type"/> is <c>null</c>.
-        /// </exception>
-        ReadOnlyCollection<IRole> GetRoles(ITopic type);
-        #endregion
+		/// <summary>
+		///     Returns all roles with the specified <c>type</c>.
+		///     The return value may be empty but must never be <c>null</c>.
+		/// </summary>
+		/// <param name="type">
+		///     The type of the <see cref="T:org.tmapi.core.IRole"/> instances to be returned, must not be <c>null</c>.
+		/// </param>
+		/// <returns>
+		///     An unmodifiable (maybe empty) set of roles with the specified <c>type</c> property.
+		/// </returns>
+		/// <exception cref="T:System.ArgumentNullException">
+		///     If the <paramref name="type"/> is <c>null</c>.
+		/// </exception>
+		ReadOnlyCollection<IRole> GetRoles(ITopic type);
+		#endregion
 	}
 }
