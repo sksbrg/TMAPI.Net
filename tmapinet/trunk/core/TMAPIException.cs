6d25872448a2d2c78a60775819f348b415fd7fdf
mhoyer <mhoyer@c94865a6-8e29-11dd-9ae4-9bd23fdad81d>
2008-10-28 23:26:22 +0000

    * fixed StyleCop warnings for TMAPIException.cs   * SA1012: Invalid spacing around the opening curly bracket.   * SA1013: Invalid spacing around the closing curly bracket.   * SA1500: If a statement spans multiple lines, the opening curly bracket must be placed on its own line.   * SA1502: The constructor must not be placed on a single line. The opening and closing curly brackets must each be placed on their own line.   * SA1505: An opening curly bracket must not be followed by a blank line.   * SA1508: A closing curly bracket must not be preceded by a blank line.   * SA1642: The documentation text within the constructor's summary tag must begin with the text: Initializes a new instance of the TMAPIException class.

--- tmapinet/trunk/core/TMAPIException.cs
+++ tmapinet/trunk/core/TMAPIException.cs
@@ -1,59 +1,54 @@
 using System;
 
-namespace org.tmapi.core {
+namespace org.tmapi.core
+{
 	/// <summary>
 	///     The base class for all standard (non run-time) exceptions thrown by a TMAPI system.
 	/// </summary>
-    [Serializable] 
-	public class TMAPIException : Exception 
-    {
-        /// <summary>
-        ///     Constructs a new exception.
-        /// </summary>
-        public TMAPIException() {}
+	[Serializable]
+	public class TMAPIException : Exception
+	{
+		#region constructor logic
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TMAPIException"/> class.
+		/// </summary>
+		public TMAPIException()
+		{
+		}
 
-        /// <summary>
-        ///     Constructs a new exception with the specified detail <paramref name="message"/>.
-        /// </summary>
-        /// <param name="message">
-        ///     The detail message.
-        /// </param>
-        public TMAPIException(string message)
-            : base(message)
-        {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TMAPIException"/> class with the specified detail <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The detail message.</param>
+		public TMAPIException(string message)
+			: base(message)
+		{
+		}
 
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TMAPIException"/> class that wraps another exception with the specified detail <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The detail message.</param>
+		/// <param name="innerException">Exception to be wrapped.</param>
+		public TMAPIException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
 
-        /// <summary>
-        ///     Constructs a new exception that wraps another exception with the specified detail <paramref name="message"/>.
-        /// </summary>
-        /// <param name="message">
-        ///     The detail message.    
-        /// </param>
-        /// <param name="innerException">
-        ///     Exception to be wrapped.
-        /// </param>
-        public TMAPIException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-            
-        }
-
-        /// <summary>
-        ///     Constructs a new exception with serialized data. 
-        /// </summary>
-        /// <param name="info">
-        ///     The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object 
-        ///     data about the exception being thrown.
-        /// </param>
-        /// <param name="context">
-        ///     The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual 
-        ///     information about the source or destination.
-        /// </param>
-        protected TMAPIException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) 
-            : base(info, context)
-        {
-            
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TMAPIException"/> class with serialized data.
+		/// </summary>
+		/// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object
+		/// data about the exception being thrown.</param>
+		/// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual
+		/// information about the source or destination.</param>
+		/// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
+		/// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
+		protected TMAPIException(
+			System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+			: base(info, context)
+		{
+		}
+		#endregion
 	}
 }
