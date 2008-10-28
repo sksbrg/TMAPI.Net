29eb1e88dfa319cd7933a53683451233f06f3d7b
mhoyer <mhoyer@c94865a6-8e29-11dd-9ae4-9bd23fdad81d>
2008-10-28 23:23:07 +0000

    * fixed StyleCop warnings for ModelConstraintException.cs   * SA1012: Invalid spacing around the opening curly bracket.   * SA1013: Invalid spacing around the closing curly bracket.   * SA1502: The constructor must not be placed on a single line. The opening and closing curly brackets must each be placed on their own line.   * SA1505: An opening curly bracket must not be followed by a blank line.   * SA1508: A closing curly bracket must not be preceded by a blank line.   * SA1642: The documentation text within the constructor's summary tag must begin with the text: Initializes a new instance of the ModelConstraintException class.

--- tmapinet/trunk/core/ModelConstraintException.cs
+++ tmapinet/trunk/core/ModelConstraintException.cs
@@ -2,60 +2,54 @@ using System;
 
 namespace org.tmapi.core
 {
-    /// <summary>
-    ///     This exception is used to report 
-    ///     <a href="http://www.isotopicmaps.org/sam/sam-model/">Topic Maps — Data Model</a> constraint violations.
-    /// </summary>
-    [Serializable]
-    public class ModelConstraintException : TMAPIRuntimeException
-    {
+	/// <summary>
+	///     This exception is used to report 
+	///     <a href="http://www.isotopicmaps.org/sam/sam-model/">Topic Maps — Data Model</a> constraint violations.
+	/// </summary>
+	[Serializable]
+	public class ModelConstraintException : TMAPIRuntimeException
+	{
+		#region constructor logic
         /// <summary>
-        ///     Constructs a new exception.
-        /// </summary>
-        public ModelConstraintException() {}
+		/// Initializes a new instance of the <see cref="ModelConstraintException"/> class.
+		/// </summary>
+		public ModelConstraintException()
+		{
+		}
 
-        /// <summary>
-        ///     Constructs a new exception with the specified detail <paramref name="message"/>.
-        /// </summary>
-        /// <param name="message">
-        ///     The detail message.
-        /// </param>
-        public ModelConstraintException(string message)
-            : base(message)
-        {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ModelConstraintException"/> class with the specified detail <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The detail message.</param>
+		public ModelConstraintException(string message)
+			: base(message)
+		{
+		}
 
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ModelConstraintException"/> class that wraps another exception with the specified detail <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The detail message.</param>
+		/// <param name="innerException">Exception to be wrapped.</param>
+		public ModelConstraintException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
 
-        /// <summary>
-        ///     Constructs a new exception that wraps another exception with the specified detail <paramref name="message"/>.
-        /// </summary>
-        /// <param name="message">
-        ///     The detail message.    
-        /// </param>
-        /// <param name="innerException">
-        ///     Exception to be wrapped.
-        /// </param>
-        public ModelConstraintException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-
-        }
-
-        /// <summary>
-        ///     Constructs a new exception with serialized data. 
-        /// </summary>
-        /// <param name="info">
-        ///     The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object 
-        ///     data about the exception being thrown.
-        /// </param>
-        /// <param name="context">
-        ///     The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual 
-        ///     information about the source or destination.
-        /// </param>
-        protected ModelConstraintException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
-            : base(info, context)
-        {
-
-        }
-    }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ModelConstraintException"/> class with serialized data.
+		/// </summary>
+		/// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object
+		/// data about the exception being thrown.</param>
+		/// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual
+		/// information about the source or destination.</param>
+		/// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
+		/// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
+		protected ModelConstraintException(
+			System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+			: base(info, context)
+		{
+		}
+		#endregion
+	}
 }
