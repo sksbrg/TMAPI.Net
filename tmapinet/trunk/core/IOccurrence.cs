393edc40fdbe77592a2c76368d3f776385137b6a
mhoyer <mhoyer@c94865a6-8e29-11dd-9ae4-9bd23fdad81d>
2008-10-28 22:38:42 +0000

    * fixed StyleCop warnings for IOccurrence.cs   * SA1500: If a statement spans multiple lines, the opening curly bracket must be placed on its own line.   * SA1623: The property's documentation summary text must begin with: Gets

--- tmapinet/trunk/core/IOccurrence.cs
+++ tmapinet/trunk/core/IOccurrence.cs
@@ -1,17 +1,22 @@
-namespace org.tmapi.core {
+namespace org.tmapi.core
+{
 	/// <summary>
 	///     Represents an 
 	///     <a href="http://www.isotopicmaps.org/sam/sam-model/#sect-occurrence">occurrence item</a>.
 	/// </summary>
-	public interface IOccurrence : IDatatypeAware, ITyped {
-        #region Properties
-        /// <summary>
-        ///     Returns the <see cref="T:org.tmapi.core.ITopic"/> to which this occurrence belongs.
-        /// </summary>
-        /// <returns>
-        ///     The <see cref="T:org.tmapi.core.ITopic"/> to which this occurrence belongs.
-        /// </returns>
-        new ITopic Parent { get; } 
-        #endregion
+	public interface IOccurrence : IDatatypeAware, ITyped
+	{
+		#region Properties
+		/// <summary>
+		///     Gets the <see cref="T:org.tmapi.core.ITopic"/> to which this occurrence belongs.
+		/// </summary>
+		/// <returns>
+		///     The <see cref="T:org.tmapi.core.ITopic"/> to which this occurrence belongs.
+		/// </returns>
+		new ITopic Parent
+		{
+			get;
+		}
+		#endregion
 	}
 }
