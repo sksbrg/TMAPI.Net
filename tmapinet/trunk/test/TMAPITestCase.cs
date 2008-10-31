83c939c95a49f97549b52eeee038c0fff405b09d
Marcel Hoyer <mhoyer@users.noreply.github.com>
2008-10-31 14:55:50 +0000

    * modified TMAPITestCase   + made it compilable by adding a lookup procedure using reflection

--- tmapinet/trunk/test/TMAPITestCase.cs
+++ tmapinet/trunk/test/TMAPITestCase.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reflection;
 using TMAPI.Net.Core;
 
 namespace TMAPI.Net.Tests
@@ -15,9 +17,31 @@ namespace TMAPI.Net.Tests
 			_system = tmf.NewTopicMapSystem();
 		}
 
+		/// <summary>
+		/// Returns a new instance of <see cref="TopicMapSystemFactory"/>.
+		/// </summary>
+		/// <remarks>
+		/// Tries to find an implementation (subclass) of <see cref="TopicMapSystemFactory"/> 
+		/// and will invoke the <see cref="TopicMapSystemFactory.NewInstance"/> method.
+		/// </remarks>
+		/// <returns>A new instance of TopicMapSystemFactory.</returns>
 		public static TopicMapSystemFactory NewTopicMapSystemFactoryInstance()
 		{
-			return TopicMapSystemFactory.NewInstance();
+			foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
+			{
+				if (type.IsSubclassOf(typeof(TopicMapSystemFactory)))
+				{
+					foreach (MethodInfo methodInfo in type.GetMethods(BindingFlags.Static | BindingFlags.Public))
+					{
+						if (methodInfo.Name == "NewInstance" && methodInfo.GetParameters().Length == 0)
+						{
+							return methodInfo.Invoke(null, null) as TopicMapSystemFactory;
+						}
+					}
+				}
+			}
+
+			throw new InvalidOperationException("You have to implement TopicMapSystemFactory first and add the reference to this project.");
 		}
 		#endregion
 	}
