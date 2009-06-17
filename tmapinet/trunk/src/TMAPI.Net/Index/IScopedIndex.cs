ff633e4a45d977ee91da48a8f3a3908c0d98a382
skesberg <sksbrg@users.noreply.github.com>
2009-06-17 12:24:42 +0000

    * Fixed code format issues according to Microsoft StyleCop

--- tmapinet/trunk/src/TMAPI.Net/Index/IScopedIndex.cs
+++ tmapinet/trunk/src/TMAPI.Net/Index/IScopedIndex.cs
@@ -1,213 +1,218 @@
-using System.Collections.ObjectModel;
-using TMAPI.Net.Core;
-
 namespace TMAPI.Net.Index
 {
-	/// <summary>
-	///     Index for <see cref="T:TMAPI.Net.Core.IScoped"/> statements and their scope. 
-	///     This index provides access to <see cref="T:TMAPI.Net.Core.IAssociation"/>s, 
-	///     <see cref="T:TMAPI.Net.Core.IOccurrence"/>s, <see cref="T:TMAPI.Net.Core.IName"/>s, 
-	///     and <see cref="T:TMAPI.Net.Core.IVariant"/>s by their scope property and to 
-	///     <see cref="T:TMAPI.Net.Core.ITopic"/>s which are used as theme in a scope.
-	/// </summary>
-	public interface IScopedIndex : IIndex
-	{
-		#region Properties
-		/// <summary>
-		///     Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
-		///     in the scope property of <see cref="T:TMAPI.Net.Core.IAssociation"/>s. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
-		/// </returns>
-		ReadOnlyCollection<ITopic> AssociationThemes
-		{
-			get;
-		}
+    using System.Collections.ObjectModel;
 
-		/// <summary>
-		///     Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
-		///     in the scope property of <see cref="T:TMAPI.Net.Core.IName"/>s. 
-		///     The return value may be empty but must never be <tt>null</tt>.
-		/// </summary>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
-		/// </returns>
-		ReadOnlyCollection<ITopic> NameThemes
-		{
-			get;
-		}
+    using TMAPI.Net.Core;
 
-		/// <summary>
-		///     Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
-		///     in the scope property of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s. 
-		///     The return value may be empty but must never be <tt>null</tt>.
-		/// </summary>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
-		/// </returns>
-		ReadOnlyCollection<ITopic> OccurrenceThemes
-		{
-			get;
-		}
+    /// <summary>
+    /// Index for <see cref="T:TMAPI.Net.Core.IScoped"/> statements and their scope. 
+    /// This index provides access to <see cref="T:TMAPI.Net.Core.IAssociation"/>s, 
+    /// <see cref="T:TMAPI.Net.Core.IOccurrence"/>s, <see cref="T:TMAPI.Net.Core.IName"/>s, 
+    /// and <see cref="T:TMAPI.Net.Core.IVariant"/>s by their scope property and to 
+    /// <see cref="T:TMAPI.Net.Core.ITopic"/>s which are used as theme in a scope.
+    /// </summary>
+    public interface IScopedIndex : IIndex
+    {
+        #region Properties
 
-		/// <summary>
-		///     Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
-		///     in the scope property of <see cref="T:TMAPI.Net.Core.IVariant"/>s. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
-		/// </returns>
-		ReadOnlyCollection<ITopic> VariantThemes
-		{
-			get;
-		}
-		#endregion
+        /// <summary>
+        /// Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
+        /// in the scope property of <see cref="T:TMAPI.Net.Core.IAssociation"/>s. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <returns>
+        ///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
+        /// </returns>
+        ReadOnlyCollection<ITopic> AssociationThemes
+        {
+            get;
+        }
 
-		#region Methods
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IAssociation"/>s in the topic map whose 
-		///     scope property contains the specified <paramref name="theme"/>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="theme">
-		///     <see cref="T:TMAPI.Net.Core.ITopic"/> which must be part of the scope. 
-		///     If it is <c>null</c> all <see cref="T:TMAPI.Net.Core.IAssociation"/>s in the 
-		///     unconstrained scope are returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IAssociation"/>s.
-		/// </returns>
-		ReadOnlyCollection<IAssociation> GetAssociations(ITopic theme);
+        /// <summary>
+        /// Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
+        /// in the scope property of <see cref="T:TMAPI.Net.Core.IName"/>s. 
+        /// The return value may be empty but must never be <tt>null</tt>.
+        /// </summary>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
+        /// </returns>
+        ReadOnlyCollection<ITopic> NameThemes
+        {
+            get;
+        }
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IAssociation"/>s in the topic map whose 
-		///     scope property equals one of those <paramref name="themes"/> at least. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="themes">
-		///     Scope of the <see cref="T:TMAPI.Net.Core.IAssociation"/>s to be returned.
-		/// </param>
-		/// <param name="matchAll">
-		///     If <c>true</c> the scope property of an association must match all <paramref name="themes"/>, 
-		///     if <c>false</c> one theme must be matched at least.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IAssociation"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="themes"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IAssociation> GetAssociations(ITopic[] themes, bool matchAll);
+        /// <summary>
+        /// Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
+        /// in the scope property of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s. 
+        /// The return value may be empty but must never be <tt>null</tt>.
+        /// </summary>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
+        /// </returns>
+        ReadOnlyCollection<ITopic> OccurrenceThemes
+        {
+            get;
+        }
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IName"/>s in the topic map whose 
-		///     scope property contains the specified <paramref name="theme"/>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="theme">
-		///     <see cref="T:TMAPI.Net.Core.ITopic"/> which must be part of the scope. 
-		///     If it is <c>null</c> all <see cref="T:TMAPI.Net.Core.IName"/>s in the 
-		///     unconstrained scope are returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IName"/>s.
-		/// </returns>
-		ReadOnlyCollection<IName> GetNames(ITopic theme);
+        /// <summary>
+        /// Gets the <see cref="T:TMAPI.Net.Core.ITopic"/>s in the topic map used 
+        /// in the scope property of <see cref="T:TMAPI.Net.Core.IVariant"/>s. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.ITopic"/>s.
+        /// </returns>
+        ReadOnlyCollection<ITopic> VariantThemes
+        {
+            get;
+        }
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IName"/>s in the topic map whose 
-		///     scope property equals one of those <paramref name="themes"/> at least. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="themes">
-		///     Scope of the <see cref="T:TMAPI.Net.Core.IName"/>s to be returned.
-		/// </param>
-		/// <param name="matchAll">
-		///     If <c>true</c> the scope property of an name must match all <paramref name="themes"/>, 
-		///     if <c>false</c> one theme must be matched at least.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IName"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="themes"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IName> GetNames(ITopic[] themes, bool matchAll);
+        #endregion
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map whose 
-		///     scope property contains the specified <paramref name="theme"/>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="theme">
-		///     <see cref="T:TMAPI.Net.Core.ITopic"/> which must be part of the scope. 
-		///     If it is <c>null</c> all <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the 
-		///     unconstrained scope are returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
-		/// </returns>
-		ReadOnlyCollection<IOccurrence> GetOccurrences(ITopic theme);
+        #region Methods
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map whose 
-		///     scope property equals one of those <paramref name="themes"/> at least. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="themes">
-		///     Scope of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
-		/// </param>
-		/// <param name="matchAll">
-		///     If <c>true</c> the scope property of an occurrence must match all <paramref name="themes"/>, 
-		///     if <c>false</c> one theme must be matched at least.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="themes"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IOccurrence> GetOccurrences(ITopic[] themes, bool matchAll);
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IAssociation"/>s in the topic map whose 
+        /// scope property contains the specified <paramref name="theme"/>. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="theme">
+        /// <see cref="T:TMAPI.Net.Core.ITopic"/>which must be part of the scope. 
+        /// If it is <c>null</c> all <see cref="T:TMAPI.Net.Core.IAssociation"/>s in the 
+        /// unconstrained scope are returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IAssociation"/>s.
+        /// </returns>
+        ReadOnlyCollection<IAssociation> GetAssociations(ITopic theme);
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
-		///     scope property contains the specified <paramref name="theme"/>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="theme">
-		///     <see cref="T:TMAPI.Net.Core.ITopic"/> which must be part of the scope. 
-		///     This must not be null <c>null</c>.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="theme"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IVariant> GetVariants(ITopic theme);
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IAssociation"/>s in the topic map whose 
+        /// scope property equals one of those <paramref name="themes"/> at least. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="themes">
+        /// Scope of the <see cref="T:TMAPI.Net.Core.IAssociation"/>s to be returned.
+        /// </param>
+        /// <param name="matchAll">
+        /// If <c>true</c> the scope property of an association must match all <paramref name="themes"/>, 
+        /// if <c>false</c> one theme must be matched at least.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IAssociation"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="themes"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IAssociation> GetAssociations(ITopic[] themes, bool matchAll);
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
-		///     scope property equals one of those <paramref name="themes"/> at least. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="themes">
-		///     Scope of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
-		/// </param>
-		/// <param name="matchAll">
-		///     If <c>true</c> the scope property of an variant must match all <paramref name="themes"/>, 
-		///     if <c>false</c> one theme must be matched at least.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="themes"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IVariant> GetVariants(ITopic[] themes, bool matchAll);
-		#endregion
-	}
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IName"/>s in the topic map whose 
+        /// scope property contains the specified <paramref name="theme"/>. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="theme">
+        /// <see cref="T:TMAPI.Net.Core.ITopic"/>which must be part of the scope. 
+        /// f it is <c>null</c> all <see cref="T:TMAPI.Net.Core.IName"/>s in the 
+        /// unconstrained scope are returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IName"/>s.
+        /// </returns>
+        ReadOnlyCollection<IName> GetNames(ITopic theme);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IName"/>s in the topic map whose 
+        /// scope property equals one of those <paramref name="themes"/> at least. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="themes">
+        /// Scope of the <see cref="T:TMAPI.Net.Core.IName"/>s to be returned.
+        /// </param>
+        /// <param name="matchAll">
+        /// If <c>true</c> the scope property of an name must match all <paramref name="themes"/>, 
+        /// if <c>false</c> one theme must be matched at least.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IName"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="themes"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IName> GetNames(ITopic[] themes, bool matchAll);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map whose 
+        /// scope property contains the specified <paramref name="theme"/>. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="theme">
+        /// <see cref="T:TMAPI.Net.Core.ITopic"/>which must be part of the scope. 
+        /// If it is <c>null</c> all <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the 
+        /// unconstrained scope are returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
+        /// </returns>
+        ReadOnlyCollection<IOccurrence> GetOccurrences(ITopic theme);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map whose 
+        /// scope property equals one of those <paramref name="themes"/> at least. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="themes">
+        /// Scope of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
+        /// </param>
+        /// <param name="matchAll">
+        /// If <c>true</c> the scope property of an occurrence must match all <paramref name="themes"/>, 
+        /// if <c>false</c> one theme must be matched at least.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="themes"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IOccurrence> GetOccurrences(ITopic[] themes, bool matchAll);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
+        /// scope property contains the specified <paramref name="theme"/>. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="theme">
+        /// <see cref="T:TMAPI.Net.Core.ITopic"/>which must be part of the scope. 
+        /// This must not be null <c>null</c>.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="theme"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IVariant> GetVariants(ITopic theme);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
+        /// scope property equals one of those <paramref name="themes"/> at least. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="themes">
+        /// Scope of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
+        /// </param>
+        /// <param name="matchAll">
+        /// If <c>true</c> the scope property of an variant must match all <paramref name="themes"/>, 
+        /// if <c>false</c> one theme must be matched at least.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="themes"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IVariant> GetVariants(ITopic[] themes, bool matchAll);
+
+        #endregion
+    }
 }
