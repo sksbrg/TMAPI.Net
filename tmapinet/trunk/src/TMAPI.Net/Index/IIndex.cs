44845dc41f9b1a8613e18720412673c37cdd3b4c
skesberg <skesberg@c94865a6-8e29-11dd-9ae4-9bd23fdad81d>
2009-06-17 12:24:42 +0000

    * Fixed code format issues according to Microsoft StyleCop

--- tmapinet/trunk/src/TMAPI.Net/Index/IIndex.cs
+++ tmapinet/trunk/src/TMAPI.Net/Index/IIndex.cs
@@ -1,59 +1,62 @@
 namespace TMAPI.Net.Index
 {
-	/// <summary>
-	///     Base interface for all indices.
-	/// </summary>
-	public interface IIndex
-	{
-		#region Properties
-		/// <summary>
-		///     Gets a value indicating whether the index is updated automatically.
-		/// </summary>
-		/// <remarks>
-		///	    If the value of this is
-		///     <c>true</c>, then the index is automatically kept synchronized with the topic
-		///     map as values are changed. If the value is <c>false</c>, then the 
-		///     <see cref="M:TMAPI.Net.Index.IIndex.Reindex"/> method must be called to 
-		///     resynchronize the index with the topic map after values are changed.
-		/// </remarks>
-		/// <returns>
-		///     <c>true</c> if index is updated automatically, <c>false</c> otherwise.
-		/// </returns>
-		bool AutoUpdated
-		{
-			get;
-		}
+    /// <summary>
+    /// Base interface for all indices.
+    /// </summary>
+    public interface IIndex
+    {
+        #region Properties
 
-		/// <summary>
-		///     Gets a value indicating whether the index is open.
-		/// </summary>
-		/// <returns>
-		///     <c>true</c> if index is already opened, <c>false</c> otherwise.
-		/// </returns>
-		bool IsOpen
-		{
-			get;
-		}
-		#endregion
+        /// <summary>
+        /// Gets a value indicating whether the index is updated automatically.
+        /// </summary>
+        /// <remarks>
+        /// If the value of this is <c>true</c>, then the index is automatically kept synchronized with the topic
+        /// map as values are changed. If the value is <c>false</c>, then the <see cref="M:TMAPI.Net.Index.IIndex.Reindex"/>
+        /// method must be called to resynchronize the index with the topic map after values are changed.
+        /// </remarks>
+        /// <returns>
+        /// <c>true</c> if index is updated automatically, <c>false</c> otherwise.
+        /// </returns>
+        bool AutoUpdated
+        {
+            get;
+        }
 
-		#region Methods
-		/// <summary>
-		///     Close the index.
-		/// </summary>
-		void Close();
+        /// <summary>
+        /// Gets a value indicating whether the index is open.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if index is already opened, <c>false</c> otherwise.
+        /// </returns>
+        bool IsOpen
+        {
+            get;
+        }
 
-		/// <summary>
-		///     Open the index.
-		///     This method must be invoked before using any other method
-		///     exported by this interface or derived interfaces.
-		/// </summary>
-		void Open();
+        #endregion
 
-		/// <summary>
-		///     Synchronize the index with data in the topic map.
-		/// </summary>
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Reindex", Justification = "Its the naming convention of TMAPI.")]
-		void Reindex();
-		#endregion
-	}
+        #region Methods
+
+        /// <summary>
+        /// Close the index.
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Open the index.
+        /// This method must be invoked before using any other method exported by this interface or derived interfaces.
+        /// </summary>
+        void Open();
+
+        /// <summary>
+        /// Synchronize the index with data in the topic map.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming",
+                "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Reindex",
+                Justification = "Its the naming convention of TMAPI.")]
+        void Reindex();
+
+        #endregion
+    }
 }
--- tmapinet/trunk/src/TMAPI.Net/Index/ILiteralIndex.cs
+++ tmapinet/trunk/src/TMAPI.Net/Index/ILiteralIndex.cs
@@ -1,139 +1,141 @@
-using System.Collections.ObjectModel;
-using TMAPI.Net.Core;
-
 namespace TMAPI.Net.Index
 {
-	/// <summary>
-	///     Index for literal values stored in a topic map.
-	/// </summary>
-	public interface ILiteralIndex : IIndex
-	{
-		#region Methods
-		/// <summary>
-		///     Retrieves the <see cref="T:TMAPI.Net.Core.IName"/>s in the topic map 
-		///     which have a value equal to <paramref name="value"/>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="value">
-		///     The value of the <see cref="T:TMAPI.Net.Core.IName"/>s to be returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IName"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="value"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IName> GetNames(string value);
+    using System.Collections.ObjectModel;
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map 
-		///     whose value property matches <paramref name="value"/> and whose datatype 
-		///     property is <a href="http://www.w3.org/TR/xmlschema-2/#string">xsd:string</a>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="value">
-		///     The value of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="value"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IOccurrence> GetOccurrences(string value);
+    using TMAPI.Net.Core;
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map 
-		///     whose value property matches the IRI represented by <paramref name="value"/>. 
-		///     Those <see cref="T:TMAPI.Net.Core.IOccurrence"/>s which have a datatype equal to 
-		///     <a href="http://www.w3.org/TR/xmlschema-2/#anyURI">xsd:anyURI</a> and their 
-		///     value property is equal to <see cref="P:TMAPI.Net.Core.ILocator.Reference"/> are returned. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="value">
-		///     The value of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="value"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IOccurrence> GetOccurrences(ILocator value);
+    /// <summary>
+    /// Index for literal values stored in a topic map.
+    /// </summary>
+    public interface ILiteralIndex : IIndex
+    {
+        #region Methods
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map 
-		///     whose value property matches <paramref name="value"/> and whose <c>datatype</c> is <paramref name="datatype"/>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="value">
-		///     The value of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
-		/// </param>
-		/// <param name="datatype">
-		///     The <c>datatype</c> of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="value"/> or <paramref name="datatype"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IOccurrence> GetOccurrences(string value, ILocator datatype);
+        /// <summary>
+        /// Retrieves the <see cref="T:TMAPI.Net.Core.IName"/>s in the topic map which have a value equal to <paramref name="value"/>.
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the <see cref="T:TMAPI.Net.Core.IName"/>s to be returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IName"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IName> GetNames(string value);
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
-		///     value property matches <paramref name="value"/> and whose <c>datatype</c> property is 
-		///     <a href="http://www.w3.org/TR/xmlschema-2/#string"><c>xsd:string</c></a>.
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="value">
-		///     The value of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="value"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IVariant> GetVariants(string value);
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map 
+        /// whose value property matches <paramref name="value"/> and whose datatype 
+        /// property is <a href="http://www.w3.org/TR/xmlschema-2/#string">xsd:string</a>. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IOccurrence> GetOccurrences(string value);
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
-		///     value property matches the IRI represented by <paramref name="value"/>. 
-		///     Those <see cref="T:TMAPI.Net.Core.IVariant"/>s which have a <c>datatype</c> equal to 
-		///     <a href="http://www.w3.org/TR/xmlschema-2/#anyURI"><c>xsd:anyURI</c></a> and their 
-		///     value property is equal to <see cref="P:TMAPI.Net.Core.ILocator.Reference"/> are returned. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="value">
-		///     The value of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="value"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IVariant> GetVariants(ILocator value);
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map 
+        /// whose value property matches the IRI represented by <paramref name="value"/>. 
+        /// Those <see cref="T:TMAPI.Net.Core.IOccurrence"/>s which have a datatype equal to 
+        /// <a href="http://www.w3.org/TR/xmlschema-2/#anyURI">xsd:anyURI</a>and their 
+        /// value property is equal to <see cref="P:TMAPI.Net.Core.ILocator.Reference"/> are returned. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IOccurrence> GetOccurrences(ILocator value);
 
-		/// <summary>
-		///     Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map 
-		///     whose value property matches <paramref name="value"/> and whose <c>datatype</c> is <paramref name="datatype"/>. 
-		///     The return value may be empty but must never be <c>null</c>.
-		/// </summary>
-		/// <param name="value">
-		///     The value of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
-		/// </param>
-		/// <param name="datatype">
-		///     The <c>datatype</c> of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
-		/// </param>
-		/// <returns>
-		///     An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
-		/// </returns>
-		/// <exception cref="T:System.ArgumentNullException">
-		///     If the <paramref name="value"/> or <paramref name="datatype"/> is <c>null</c>.
-		/// </exception>
-		ReadOnlyCollection<IVariant> GetVariants(string value, ILocator datatype);
-		#endregion
-	}
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s in the topic map 
+        /// whose value property matches <paramref name="value"/> and whose <c>datatype</c> is <paramref name="datatype"/>. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
+        /// </param>
+        /// <param name="datatype">
+        /// The <c>datatype</c> of the <see cref="T:TMAPI.Net.Core.IOccurrence"/>s to be returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IOccurrence"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="value"/> or <paramref name="datatype"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IOccurrence> GetOccurrences(string value, ILocator datatype);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
+        /// value property matches <paramref name="value"/> and whose <c>datatype</c> property is 
+        /// <a href="http://www.w3.org/TR/xmlschema-2/#string"><c>xsd:string</c></a>.
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IVariant> GetVariants(string value);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map whose 
+        /// value property matches the IRI represented by <paramref name="value"/>. 
+        /// Those <see cref="T:TMAPI.Net.Core.IVariant"/>s which have a <c>datatype</c> equal to 
+        /// <a href="http://www.w3.org/TR/xmlschema-2/#anyURI"><c>xsd:anyURI</c></a>and their 
+        /// value property is equal to <see cref="P:TMAPI.Net.Core.ILocator.Reference"/> are returned. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IVariant> GetVariants(ILocator value);
+
+        /// <summary>
+        /// Returns the <see cref="T:TMAPI.Net.Core.IVariant"/>s in the topic map 
+        /// whose value property matches <paramref name="value"/> and whose <c>datatype</c> is <paramref name="datatype"/>. 
+        /// The return value may be empty but must never be <c>null</c>.
+        /// </summary>
+        /// <param name="value">
+        /// The value of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
+        /// </param>
+        /// <param name="datatype">
+        /// The <c>datatype</c> of the <see cref="T:TMAPI.Net.Core.IVariant"/>s to be returned.
+        /// </param>
+        /// <returns>
+        /// An unmodifiable collection of <see cref="T:TMAPI.Net.Core.IVariant"/>s.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// If the <paramref name="value"/> or <paramref name="datatype"/> is <c>null</c>.
+        /// </exception>
+        ReadOnlyCollection<IVariant> GetVariants(string value, ILocator datatype);
+
+        #endregion
+    }
 }
