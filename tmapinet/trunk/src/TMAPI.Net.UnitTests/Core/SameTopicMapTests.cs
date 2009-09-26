6362dc57566290969c85a4e1ae6e2b1a91a870da
skesberg <sksbrg@users.noreply.github.com>
2009-09-26 10:15:27 +0000

    * Added copyright information

--- tmapinet/trunk/src/TMAPI.Net.UnitTests/Core/SameTopicMapTests.cs
+++ tmapinet/trunk/src/TMAPI.Net.UnitTests/Core/SameTopicMapTests.cs
@@ -1,4 +1,22 @@
-﻿namespace TMAPI.Net.UnitTests.Core
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SameTopicMapTests.cs">
+//  TMAPI.Net was created collectively by the membership of the tmapinet-discuss mailing list 
+//  (https://lists.sourceforge.net/lists/listinfo/tmapinet-discuss) with support by the 
+//  tmapi-discuss mailing list (http://lists.sourceforge.net/mailman/listinfo/tmapi-discuss),
+//  and is hereby released into the public domain; and comes with NO WARRANTY.
+//  
+//  No one owns TMAPI.Net: you may use it freely in both commercial and
+//  non-commercial applications, bundle it with your software
+//  distribution, include it on a CD-ROM, list the source code in a
+//  book, mirror the documentation at your own web site, or use it in
+//  any other way you see fit.
+// </copyright>
+// <summary>
+//   Checks the "same topic map" constraint.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TMAPI.Net.UnitTests.Core
 {
     using System.Collections.Generic;
     using Net.Core;
